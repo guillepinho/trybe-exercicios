fb75773d87856e96b6469270d87df6435621c447
guillepinho <guilherme.apinho@hotmail.com>
2022-05-19 16:48:02 -0300

    adding exercises made during the classes

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/seletores-de-elementos/f1-classnames/script.js
@@ -0,0 +1,5 @@
+let pilotosDeF1 = document.getElementsByClassName('piloto-f1-atual');
+
+for (let index in pilotosDeF1) {
+    pilotosDeF1[index].innerText = "Lewis Hamilton"
+}
---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/seletores-de-elementos/querySelector/script.js
@@ -0,0 +1,3 @@
+document.querySelector('.pilotos-sem-titulo p').innerText = 'Selected';
+
+document.querySelectorAll('.pilotos-sem-titulo .piloto-f1-atual')[0].innerText = 'Selected through all';
