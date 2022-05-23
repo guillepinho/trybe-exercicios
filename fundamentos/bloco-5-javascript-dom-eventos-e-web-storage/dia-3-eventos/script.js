8a60c1951c158b4c26cce535aff73fd43313f3e1
guillepinho <guilherme.apinho@hotmail.com>
2022-05-23 18:30:12 -0300

    First commit of Exercises/5.3

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-eventos/script.js
@@ -0,0 +1,52 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+
+    for (let index = 0; index < weekDays.length; index += 1) {
+        const days = weekDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+
+        weekDaysList.appendChild(dayListItem);
+    };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+// 🚀 Exercício 1:
+// O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days". Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+// Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>
+// Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
+// Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li>
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dezHolidays = [24, 25, 31];
+const dezFridays = [4, 11, 18, 25];
+
+function addDays(days, holidays, fridays) {
+    const dayList = document.querySelector('#days');
+
+    for (let day of days) {
+        const newDay = document.createElement('li');
+        newDay.innerText = day;
+        newDay.classList.add('day');
+    
+        if (holidays.includes(day)) {
+            newDay.classList.add('holiday');
+        }
+        if (fridays.includes(day)) {
+            newDay.classList.add('friday');
+        }
+    
+        dayList.appendChild(newDay);
+    }
+}
+
+addDays(dezDaysList, dezHolidays, dezFridays);
+
+
+
+
+
+
+
