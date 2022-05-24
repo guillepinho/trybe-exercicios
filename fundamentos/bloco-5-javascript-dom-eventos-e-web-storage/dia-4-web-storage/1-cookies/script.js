0a252bc7879dc74fa27c86d603cd177f36412e86
guillepinho <guilherme.apinho@hotmail.com>
2022-05-24 15:30:28 -0300

    days examples

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-web-storage/1-cookies/script.js
@@ -0,0 +1,5 @@
+document.cookie = 'email=email@gmail.com';
+const myCookie = document.cookie;
+console.log(myCookie);
+
+document.cookie = 'email=email@gmail.com; expires:Thu, 17 Dec 2020 12:00:00 UTC';
