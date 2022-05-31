5b89a97434a19d79ad5f760b34bb0117e21cc5bb
guillepinho <guilherme.apinho@hotmail.com>
2022-05-31 16:00:56 -0300

    commit das aulas e exercicios de fixação

---
+++ fundamentos/bloco-6-html-css-forms-flexbox-responsivo/dia-1-html-css-forms/4-preventDefault/exercicio/script.js
@@ -0,0 +1,11 @@
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+function prevent(event) {
+    event.preventDefault();
+}
+
+HREF_LINK.addEventListener('click', prevent);
+INPUT_CHECKBOX.addEventListener('click', prevent);
+INPUT_TEXT.addEventListener('keypress', prevent);
