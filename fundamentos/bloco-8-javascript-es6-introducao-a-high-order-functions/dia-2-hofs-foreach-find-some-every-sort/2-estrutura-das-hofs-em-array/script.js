8b4aa22f908db5b7af40783873ca19471e686cf1
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-06-23 15:50:25 -0300

    Conteudo 8.2

---
+++ fundamentos/bloco-8-javascript-es6-introducao-a-high-order-functions/dia-2-hofs-foreach-find-some-every-sort/2-estrutura-das-hofs-em-array/script.js
@@ -0,0 +1,37 @@
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element) => {
+    console.log('Cada elemento do array:', element);
+});
+
+// Cada elemento do array: josé
+// Cada elemento do array: 50
+// Cada elemento do array: 0.25
+// Cada elemento do array: { comida: 'Chocolate' }
+
+// arrayOfValues - Nome do array que será percorrido;
+// .forEach - A HOF, pode ser, .find, .some, .every;
+// element - Valor do elemento do array;
+// (element) => {console.log('Cada elemento do array:', element); - função a ser executada, pode ser passada igual ao terceiro exemplo do conteúdo com a função verifyGrade.
+
+arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
+    console.log('Cada elemento do array:', element);
+    console.log('Index, posição do elemento:', indexOfTheArray);
+    console.log('Array percorrido:', theEntireArray);
+});
+
+// => ---------------
+//   Cada elemento do array: josé
+//   Index, posição do elemento: 0
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 50
+//   Index, posição do elemento: 1
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 0.25
+//   Index, posição do elemento: 2
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: { comida: 'Chocolate' }
+//   Index, posição do elemento: 3
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
