fe0f6c228f844f1d3d6c794459a96e4e6072f420
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-08-11 14:43:45 -0300

    fix

--- front-end/bloco-1-Javascript-e-testes-assincronos/dia-3-testes-assincronos/1-testando-cod-assincrono/consultaCEP.test.js
+++
@@ -1,7 +0,0 @@
-const { consultaCEP } = require('./consultaCEP.js');
-
-describe('Testes da função de Consulta ao CEP', () => {
-    test('Verifica se a função retorna uma string contendo o endereço completo do CEP', async () => {
-        expect(await consultaCEP('51021300')).toEqual('CEP: 51021-300, Rua Jorge Couceiro da Costa Eiras, no bairro de Boa Viagem, na cidade de Recife/PE.');
-    })
-});
