071e679cc382c9157330dc1d5bd3bcb76b8dba01
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-08-15 15:54:07 -0300

    14.3

---
+++ front-end/bloco-14-RTL-react-testing-library/dia-3-testando-react-router/1-testando-react-router/testando-router/src/App.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App, { About } from './App';
+import { act } from 'react-dom/test-utils';
+
+describe(':: teste da aplicação toda', () => {
+  it('deve renderizar o componente App', () => {
+    renderWithRouter(<App />);
+
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  it('deve renderizar o componente About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+
+    expect(homeTitle).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
+    const aboutTitle = screen.getByRole('heading', {
+      name: 'Você está na página Sobre',
+    });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('deve testar um caminho não existente e a renderização do Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/pagina/que-nao-existe/');
+    });
+
+    const notFoundTitle = screen.getByRole('heading',
+      { name: 'Página não encontrada' });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+});
