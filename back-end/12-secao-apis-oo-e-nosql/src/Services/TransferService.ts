4aae3bce70b6f8cd36aaac50f7a429c7d3a4d2f7
Guilherme Pinho <guilherme.apinho@hotmail.com>
2023-02-21 18:47:15 -0300

    feat(120secao done): added files for 12-secao

---
+++ back-end/12-secao-apis-oo-e-nosql/src/Services/TransferService.ts
@@ -0,0 +1,41 @@
+import Payment from '../Domain/Payment/Payment';
+import IPayment from '../Interfaces/IPayment';
+import PaymentODM from '../Models/PaymentODM';
+import KeyService from './KeyService';
+
+class TransferService {
+  private createPaymentDomain(payment: IPayment | null): Payment | null {
+    if (payment) {
+      return new Payment(
+        payment.payByPerson,
+        payment.payToPerson,
+        payment.amount,
+        payment.key,
+        payment.id,
+        payment.status,
+      );
+    }
+    return null;
+  }
+
+  public async transfer(payment: IPayment) {
+    const keyService = new KeyService();
+    if (await keyService.getByValue(payment.key)) {
+      const paymentODM = new PaymentODM();
+      const newPayment = await paymentODM.create(payment);
+      return this.createPaymentDomain(newPayment);
+    }
+    throw new Error('Key not found');
+  }
+
+  public async undoTransfer(id: string, payment: IPayment) {
+    const keyService = new KeyService();
+    if (await keyService.getByValue(payment.key)) {
+      const paymentODM = new PaymentODM();
+      return paymentODM.update(id, payment);
+    }
+    throw new Error('Key not found');
+  }
+}
+
+export default TransferService;
---
+++ back-end/12-secao-apis-oo-e-nosql/tests/unit/services/transfer.test.ts
@@ -0,0 +1,64 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import IPayment from '../../../src/Interfaces/IPayment';
+import PaymentStatus from '../../../src/utils/PaymentStatus';
+import TransferService from '../../../src/Services/TransferService';
+import Payment from '../../../src/Domain/Payment/Payment';
+import Key from '../../../src/Domain/Key/Key';
+
+describe('Deveria criar uma transferência TRIX', function () {
+  it('Deveria criar uma transferência TRIX com SUCESSO', async function () {
+    const cpf = '858.898.670-16';
+    const paymentInput: IPayment = {
+      payByPerson: 'Jobs',
+      payToPerson: 'Wozniak',
+      amount: 50000,
+      key: cpf,
+    };
+    const outputPayment: Payment = new Payment(
+      'Jobs',
+      'Wozniak',
+      50000,
+      cpf,
+      '63319d80feb9f483ee823ac5',
+      PaymentStatus.concluded,
+    );
+    const outputKey: Key = new Key(
+      cpf,
+      'Wozniak',
+      'cpf',
+      '633ec9fa3df977e30e993492',
+    );
+    sinon.stub(Model, 'create').resolves(outputPayment);
+    sinon.stub(Model, 'findOne').resolves(outputKey);
+
+    const service = new TransferService();
+    const result = await service.transfer(paymentInput);
+
+    expect(result).to.be.deep.equal(outputPayment);
+  });
+
+  it('Deveria lançar uma exceção quando a key não existe', async function () {
+    const paymentInput: IPayment = {
+      payByPerson: 'Jobs',
+      payToPerson: 'Wozniak',
+      amount: 50000,
+      key: '858.898.670-16XX',
+    };
+
+    sinon.stub(Model, 'create').resolves({});
+    sinon.stub(Model, 'findOne').resolves(undefined);
+
+    try {
+      const service = new TransferService();
+      await service.transfer(paymentInput);
+    } catch (error) {
+      expect((error as Error).message).to.be.equal('Key not found');
+    }
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
